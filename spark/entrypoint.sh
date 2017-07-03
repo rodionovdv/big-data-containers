bd11c937e4e577ced6c5b550c6ed6eebd6c40baf
Dmitry Rodionov <rodionovdv@gmail.com>
2017-07-03 09:55:50 +0200

    add flink to spark container

--- spark/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server/
-
-# start spark and mesodocker rmi -f s
-supervisord -n -c /usr/local/supervisor/supervisord.conf
